7f52b97d21825ff0d0641ace1a7861b4125a26a3
Axel Ramiro <axel@alchimia.mx>
2021-07-05 17:53:31 -0500

    Polling sample

--- src/main/java/com/example/MySpringBootRouter.java
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -2,8 +2,9 @@ package com.example;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
-import org.apache.camel.model.rest.RestBindingMode;
-import org.apache.camel.Processor;
+
+import java.net.URLEncoder;
+
 import org.apache.camel.Exchange;
 
 /**
@@ -16,55 +17,26 @@ public class MySpringBootRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-
-        // restConfiguration()
-        // .component("servlet")
-        // .bindingMode(RestBindingMode.auto);
-
-        // rest("/api")
-        // // Servicio Post
-        // .post()
-        // .type(BeanDatos.class)
-        // .route()
-        // .log("The body is ${body.name}!!")
-        // .transform().simple("The name in body is ${body.saludo()}")
-        // .endRest()
-        // // Servicio Post2
-        // .post("/datos")
-        // .type(BeanDatos.class)
-        // .route()
-        // .log("The body is ${body.name}!!")
-        // .process(new Processor() {
-        //     @Override
-        //     public void process(Exchange exchange) throws Exception {
-        //         BeanDatos beanDatos = exchange.getMessage().getBody(BeanDatos.class);
-        //         beanDatos.setEdad("15");
-        //         exchange.getMessage().setBody(beanDatos);
-        //     }
-        // })
-        // .endRest()
-        // // Servicio Get
-        // .get()
-        // .route()
-        // .log("The body is ${body}")
-        // .transform().simple("The body is ${body}")
-        // .endRest();
-
-        from("timer:hello?period={{timer.period}}").routeId("hello")
-            .transform().method("myBean", "saySomething")
-            .filter(simple("${body} contains 'foo'"))
-                .to("log:foo")
-            .end()
-            .to("stream:out")
-            .to("https://run.mocky.io/v3/73666d43-8da3-4b02-b648-8121e80c4232")
-//            .to("https://run.mocky.io/v3/4e1f861e-6a7f-4ca1-85e7-e35bceee5b51")
-//            .to("stream:header")
-	        .choice()
-	        .when(simple("${header.CamelHttpResponseCode} == '200'"))
-	        	.to("stream:out")
-	        .otherwise()
-	            .log("No tenia datos la respuesta");
-//        http://10.1.99.27:8080/integrator/receipt-simple-confirm?between={"start": "2018-05-01", "end": "2018-06-12"}
+    	
+    	String wmsUri = "http://10.1.99.27:8080/integrator/receipt-simple-confirm?between=";
+    	String dateRange = WmsParams.getDateRange(60 * 60 * 24 * 90); // Poll interval in seconds (3 months)
+    	String encodedDateRange = URLEncoder.encode(dateRange, "UTF-8");
+    	wmsUri += encodedDateRange;
+    	String erpUri = "https://5298967-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=589&deploy=1";
+    	String authHeader = OAuthSign.getAuthHeader();
+    	
+    	from("timer:hello?period={{timer.period}}").routeId("poll")
+    		.to("log:DEBUG?showBody=true&showHeaders=true")
+    		.to(wmsUri)
+        	.to("log:DEBUG?showBody=true&showHeaders=true")
+        	.setHeader("CamelHttpMethod", constant("POST"))
+        	.setHeader("Authorization", simple(authHeader))
+        	.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+        	.setHeader("Accept", constant("application/json"))
+        	.to("log:DEBUG?showBody=true&showHeaders=true")
+        	.to(erpUri + "&throwExceptionOnFailure=false")
+        	.to("log:DEBUG?showBody=true&showHeaders=true")
+        	.to("stream:out");
     }
 
 }
---
+++ src/main/java/com/example/WmsParams.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.json.JSONObject;
+
+public class WmsParams {
+	
+	public static String getDateRange (int secondsAgo) {
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+		Date startDate;
+		Date endDate;
+		JSONObject dates = new JSONObject();
+
+		Date now = new Date(System.currentTimeMillis());
+      	Calendar c = Calendar.getInstance();
+        c.setTime(now);
+        c.add(Calendar.SECOND, secondsAgo * -1);
+        startDate = c.getTime();
+        endDate = now;
+      	
+      	dates.put("start", formatter.format(startDate));
+        dates.put("end", formatter.format(endDate));
+
+        return dates.toString();
+      }
+}
