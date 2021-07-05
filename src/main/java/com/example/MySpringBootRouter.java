32802242d7b2b01e751facb87485c25b5718ee83
Axel Ramiro <axel@alchimia.mx>
2021-07-05 18:24:14 -0500

    Try to gen OAuth headers on every route run

--- src/main/java/com/example/MySpringBootRouter.java
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.net.URLEncoder;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 
 /**
  * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
@@ -23,16 +24,21 @@ public class MySpringBootRouter extends RouteBuilder {
     	String encodedDateRange = URLEncoder.encode(dateRange, "UTF-8");
     	wmsUri += encodedDateRange;
     	String erpUri = "https://5298967-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=589&deploy=1";
-    	String authHeader = OAuthSign.getAuthHeader();
+
     	
     	from("timer:hello?period={{timer.period}}").routeId("poll")
     		.to("log:DEBUG?showBody=true&showHeaders=true")
     		.to(wmsUri)
         	.to("log:DEBUG?showBody=true&showHeaders=true")
         	.setHeader("CamelHttpMethod", constant("POST"))
-        	.setHeader("Authorization", simple(authHeader))
+        	.process(new Processor() {
+                @Override
+                public void process(Exchange exchange) throws Exception {
+                	String authHeader = OAuthSign.getAuthHeader();
+                    exchange.getMessage().setHeader("Authorization", authHeader);
+                }
+        	})
         	.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-        	.setHeader("Accept", constant("application/json"))
         	.to("log:DEBUG?showBody=true&showHeaders=true")
         	.to(erpUri + "&throwExceptionOnFailure=false")
         	.to("log:DEBUG?showBody=true&showHeaders=true")
